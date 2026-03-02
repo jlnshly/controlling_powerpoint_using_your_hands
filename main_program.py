d32fcfce0bbfbf3ebc573d4a6ed8d2301640b71f
jlnshly <jelaineashleyramos@gmail.com>
2026-03-02 19:07:38 +0800

    Video capture setup code

--- main_program.py
+++ main_program.py
@@ -3,9 +3,14 @@ import mediapipe as mp
 import pyautogui
 import time
 
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(
+mp_hands = mp.solutions.user_hands
+user_hands = mp_hands.Hands(
     max_num_hands=1,
     min_detection_confidence=0.7,
 )
 mp_draw = mp.solutions.drawing_utils
+
+video_capture = cv2.VideoCapture(0)
+
+
+
