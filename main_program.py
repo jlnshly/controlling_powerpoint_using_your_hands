5d9635f6607fe8fffab225fd48f3b94004392f3d
jlnshly <jelaineashleyramos@gmail.com>
2026-03-02 19:05:13 +0800

    Setting mediapipe first

--- main_program.py
+++ main_program.py
@@ -3,3 +3,9 @@ import mediapipe as mp
 import pyautogui
 import time
 
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    max_num_hands=1,
+    min_detection_confidence=0.7,
+)
+mp_draw = mp.solutions.drawing_utils
