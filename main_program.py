dec612bba36d336d05f228d71d989efbe65b75f2
jlnshly <jelaineashleyramos@gmail.com>
2026-03-03 00:07:59 +0800

    Changed proper capitalization for functions

--- main_program.py
+++ main_program.py
@@ -4,13 +4,10 @@ import pyautogui
 import time
 
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(
-    max_num_hands=1,
-    min_detection_confidence=0.7,
-)
+hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7,)
 mp_draw = mp.solutions.drawing_utils
 
-video_capture = cv2.video_capture(0)
+video_capture = cv2.VideoCapture(0)
 
 prev_action_time = 0
 cooldown_time = 1.0
@@ -25,8 +22,8 @@ def count_fingers(hand_landmarks):
         if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[tip -2].y:
             raised_fingers += 1
 
-    if hand_landmarks.landmarks[4].x < hand_landmarks.landmarks[3].x:
-        raised_fingers += 1
+        if hand_landmarks.landmark[4].x < hand_landmarks.landmark[3].x:
+            raised_fingers += 1
 
     return raised_fingers
 
@@ -37,7 +34,7 @@ while True:
         break
 
     frame = cv2.flip(frame, 1)
-    rgb_image = cv2.cvt_color(frame, cv2.COLOR_BGR2RGB)
+    rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(rgb_image)
     current_time = time.time()
 
@@ -45,7 +42,7 @@ while True:
 
         hand = results.multi_hand_landmarks[0]
 
-        mp_draw.draw_landmarks(frame, hand, mp_hands.hand_connections)
+        mp_draw.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS)
 
         finger_count = count_fingers(hand)
 
@@ -86,6 +83,7 @@ while True:
 
         cv2.imshow("Hand Controlled Powerpoint", frame)
 
+        cv2.imshow("Hand Tracking", frame)
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
